4a1fe53961f1405c88034811256d0fd02a439fd9
Ben Navetta <ben.navetta@gmail.com>
2014-01-03 14:48:14 -0500

    complete Matrix4 and add tests for it

--- src/main/java/galu/matrix/Matrix4.java
+++ src/main/java/galu/matrix/Matrix4.java
@@ -1,96 +1,341 @@
 package galu.matrix;
 
+import galu.vector.Vector4;
+
 import java.nio.FloatBuffer;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 
 public final class Matrix4 implements Matrix<Matrix4>
 {
+	public final float m00, m01, m02, m03,
+	                   m10, m11, m12, m13,
+	                   m20, m21, m22, m23,
+	                   m30, m31, m32, m33;
+
+	public Matrix4(float m00, float m01, float m02, float m03, float m10, float m11, float m12, float m13, float m20, float m21, float m22, float m23, float m30, float m31, float m32, float m33)
+	{
+		this.m00 = m00;
+		this.m01 = m01;
+		this.m02 = m02;
+		this.m03 = m03;
+		this.m10 = m10;
+		this.m11 = m11;
+		this.m12 = m12;
+		this.m13 = m13;
+		this.m20 = m20;
+		this.m21 = m21;
+		this.m22 = m22;
+		this.m23 = m23;
+		this.m30 = m30;
+		this.m31 = m31;
+		this.m32 = m32;
+		this.m33 = m33;
+	}
+
 	@Override
 	public int rowCount()
 	{
-		return 0;
+		return 4;
 	}
 
 	@Override
 	public int columnCount()
 	{
-		return 0;
+		return 4;
 	}
 
 	@Override
 	public float determinant()
 	{
-		return 0;
+		return m00*(m11*(m22*m33-m32*m23) - m12*(m21*m33-m31*m23) + m13*(m21*m32-m31*m22))
+	         - m01*(m10*(m22*m33-m32*m23) - m12*(m20*m33-m30*m23) + m13*(m20*m32-m30*m22))
+			 + m02*(m10*(m21*m33-m31*m23) - m11*(m20*m33-m30*m23) + m13*(m20*m31-m30*m21))
+			 - m03*(m10*(m21*m32-m22*m31) - m11*(m20*m32-m30*m22) + m12*(m20*m31-m30*m21));
 	}
 
+	// http://www.cg.info.hiroshima-cu.ac.jp/~miyazaki/knowledge/teche23.html
 	@Override
 	public Matrix4 inverse()
 	{
-		return null;
+		float det = determinant();
+		checkState(det != 0.0, "Matrix is not invertible: determinant is 0");
+		float factor = 1.0f / det;
+		return new Matrix4(
+			factor * (m11*m22*m33 + m12*m23*m31 + m13*m21*m32 - m11*m23*m32 - m12*m21*m33 - m13*m22*m31),
+		    factor * (m01*m23*m32 + m02*m21*m33 + m03*m22*m31 - m01*m22*m33 - m02*m23*m31 - m03*m21*m32),
+		    factor * (m01*m12*m33 + m02*m13*m31 + m03*m11*m32 - m01*m13*m32 - m02*m11*m33 - m03*m12*m31),
+		    factor * (m01*m13*m22 + m02*m11*m23 + m03*m12*m21 - m01*m12*m23 - m02*m13*m21 - m03*m11*m22),
+		    factor * (m10*m23*m32 + m12*m20*m33 + m13*m22*m30 - m10*m22*m33 - m12*m23*m30 - m13*m20*m32),
+		    factor * (m00*m22*m33 + m02*m23*m30 + m03*m20*m32 - m00*m23*m32 - m02*m20*m33 - m03*m22*m30),
+		    factor * (m00*m13*m32 + m02*m10*m33 + m03*m12*m30 - m00*m12*m33 - m02*m13*m30 - m03*m10*m32),
+		    factor * (m00*m12*m23 + m02*m13*m20 + m03*m10*m22 - m00*m13*m22 - m02*m10*m23 - m03*m12*m20),
+		    factor * (m10*m21*m33 + m11*m23*m30 + m13*m20*m31 - m10*m23*m31 - m11*m20*m33 - m13*m21*m30),
+		    factor * (m00*m23*m31 + m01*m20*m33 + m03*m21*m30 - m00*m21*m33 - m01*m23*m30 - m03*m20*m31),
+		    factor * (m00*m11*m33 + m01*m13*m30 + m03*m10*m31 - m00*m13*m31 - m01*m10*m33 - m03*m11*m30),
+		    factor * (m00*m13*m21 + m01*m10*m23 + m03*m11*m20 - m00*m11*m23 - m01*m13*m20 - m03*m10*m21),
+		    factor * (m10*m22*m31 + m11*m20*m32 + m12*m21*m30 - m10*m21*m32 - m11*m22*m30 - m12*m20*m31),
+		    factor * (m00*m21*m32 + m01*m22*m30 + m02*m20*m31 - m00*m22*m31 - m01*m20*m32 - m02*m21*m30),
+		    factor * (m00*m12*m31 + m01*m10*m32 + m02*m11*m30 - m00*m11*m32 - m01*m12*m30 - m02*m10*m31),
+		    factor * (m00*m11*m22 + m01*m12*m20 + m02*m10*m21 - m00*m12*m21 - m01*m10*m22 - m02*m11*m20)
+		);
 	}
 
 	@Override
 	public Matrix4 negate()
 	{
-		return null;
+		return new Matrix4(
+		    -m00, -m01, -m02, -m03,
+		    -m10, -m11, -m12, -m13,
+		    -m20, -m21, -m22, -m23,
+		    -m30, -m31, -m32, -m33
+		);
 	}
 
 	@Override
 	public Matrix4 transpose()
 	{
-		return null;
+		return new Matrix4(
+		    m00, m10, m20, m30,
+		    m01, m11, m21, m31,
+		    m02, m12, m22, m32,
+		    m03, m13, m23, m33
+		);
 	}
 
 	@Override
 	public Matrix4 add(Matrix4 other)
 	{
-		return null;
+		return new Matrix4(
+			m00 + other.m00, m01 + other.m01, m02 + other.m02, m03 + other.m03,
+		    m10 + other.m10, m11 + other.m11, m12 + other.m12, m13 + other.m13,
+		    m20 + other.m20, m21 + other.m21, m22 + other.m22, m23 + other.m23,
+		    m30 + other.m30, m31 + other.m31, m32 + other.m32, m33 + other.m33
+		);
 	}
 
 	@Override
 	public Matrix4 subtract(Matrix4 other)
 	{
-		return null;
+		return new Matrix4(
+				m00 - other.m00, m01 - other.m01, m02 - other.m02, m03 - other.m03,
+				m10 - other.m10, m11 - other.m11, m12 - other.m12, m13 - other.m13,
+				m20 - other.m20, m21 - other.m21, m22 - other.m22, m23 - other.m23,
+				m30 - other.m30, m31 - other.m31, m32 - other.m32, m33 - other.m33
+		);
 	}
 
 	@Override
 	public Matrix4 multiply(Matrix4 other)
 	{
-		return null;
+		return new Matrix4(
+			m00*other.m00 + m01*other.m10 + m02*other.m20 + m03*other.m30,
+		    m00*other.m01 + m01*other.m11 + m02*other.m21 + m03*other.m31,
+		    m00*other.m02 + m01*other.m12 + m02*other.m22 + m03*other.m32,
+		    m00*other.m03 + m01*other.m13 + m02*other.m23 + m03*other.m33,
+
+		    m10*other.m00 + m11*other.m10 + m12*other.m20 + m13*other.m30,
+		    m10*other.m01 + m11*other.m11 + m12*other.m21 + m13*other.m31,
+		    m10*other.m02 + m11*other.m12 + m12*other.m22 + m13*other.m32,
+		    m10*other.m03 + m11*other.m13 + m12*other.m23 + m13*other.m33,
+
+		    m20*other.m00 + m21*other.m10 + m22*other.m20 + m23*other.m30,
+		    m20*other.m01 + m21*other.m11 + m22*other.m21 + m23*other.m31,
+		    m20*other.m02 + m21*other.m12 + m22*other.m22 + m23*other.m32,
+		    m20*other.m03 + m21*other.m13 + m22*other.m23 + m23*other.m33,
+
+		    m30*other.m00 + m31*other.m10 + m32*other.m20 + m33*other.m30,
+		    m30*other.m01 + m31*other.m11 + m32*other.m21 + m33*other.m31,
+		    m30*other.m02 + m31*other.m12 + m32*other.m22 + m33*other.m32,
+		    m30*other.m03 + m31*other.m13 + m32*other.m23 + m33*other.m33
+		);
 	}
 
 	@Override
 	public Matrix4 divide(Matrix4 other)
 	{
-		return null;
+		return multiply(other.inverse());
 	}
 
 	@Override
 	public Matrix4 multiply(float scalar)
 	{
-		return null;
+		return new Matrix4(
+			scalar*m00, scalar*m01, scalar*m02, scalar*m03,
+		    scalar*m10, scalar*m11, scalar*m12, scalar*m13,
+		    scalar*m20, scalar*m21, scalar*m22, scalar*m23,
+		    scalar*m30, scalar*m31, scalar*m32, scalar*m33
+		);
 	}
 
 	@Override
 	public Matrix4 elementMultiply(Matrix4 other)
 	{
-		return null;
+		return new Matrix4(
+				m00 * other.m00, m01 * other.m01, m02 * other.m02, m03 * other.m03,
+				m10 * other.m10, m11 * other.m11, m12 * other.m12, m13 * other.m13,
+				m20 * other.m20, m21 * other.m21, m22 * other.m22, m23 * other.m23,
+				m30 * other.m30, m31 * other.m31, m32 * other.m32, m33 * other.m33
+		);
 	}
 
 	@Override
 	public Matrix4 elementDivide(Matrix4 other)
 	{
-		return null;
+		return new Matrix4(
+			m00 / other.m00, m01 / other.m01, m02 / other.m02, m03 / other.m03,
+			m10 / other.m10, m11 / other.m11, m12 / other.m12, m13 / other.m13,
+			m20 / other.m20, m21 / other.m21, m22 / other.m22, m23 / other.m23,
+			m30 / other.m30, m31 / other.m31, m32 / other.m32, m33 / other.m33
+		);
+	}
+
+	public Vector4 transform(Vector4 vec)
+	{
+		float x = m00 * vec.x + m01 * vec.y + m02 * vec.z + m03 * vec.w;
+		float y = m10 * vec.x + m11 * vec.y + m12 * vec.z + m13 * vec.w;
+		float z = m20 * vec.x + m21 * vec.y + m22 * vec.z + m23 * vec.w;
+		float w = m30 * vec.x + m31 * vec.y + m32 * vec.z + m33 * vec.w;
+		return new Vector4(x, y, z, w);
 	}
 
 	@Override
 	public void store(FloatBuffer buf, Order order)
 	{
-
+		checkArgument(buf.remaining() >= 16, "Buffer has fewer than 16 elements remaining (%s)", buf);
+		switch(order)
+		{
+			case ROW_MAJOR:
+				buf.put(m00).put(m01).put(m02).put(m03)
+				   .put(m10).put(m11).put(m12).put(m13)
+				   .put(m20).put(m21).put(m21).put(m23)
+				   .put(m30).put(m31).put(m32).put(m33);
+				break;
+			case COLUMN_MAJOR:
+				buf.put(m00).put(m10).put(m20).put(m30)
+				   .put(m01).put(m11).put(m21).put(m31)
+				   .put(m02).put(m12).put(m22).put(m32)
+				   .put(m03).put(m13).put(m23).put(m33);
+				break;
+			default:
+				throw new IllegalArgumentException("Unsupported matrix ordering: " + order);
+		}
 	}
 
 	@Override
 	public void store(float[] array, Order order)
 	{
+		checkArgument(array.length >= 16, "Array has fewer than 16 elements (%d)", array.length);
+		switch(order)
+		{
+			case ROW_MAJOR:
+				array[0] = m00;
+				array[1] = m01;
+				array[2] = m02;
+				array[3] = m03;
 
+				array[4] = m10;
+				array[5] = m11;
+				array[6] = m12;
+				array[7] = m13;
+
+				array[8] = m20;
+				array[9] = m21;
+				array[10] = m22;
+				array[11] = m23;
+
+				array[12] = m30;
+				array[13] = m31;
+				array[14] = m32;
+				array[15] = m33;
+				break;
+			case COLUMN_MAJOR:
+				array[0] = m00;
+				array[1] = m10;
+				array[2] = m20;
+				array[3] = m30;
+
+				array[4] = m01;
+				array[5] = m11;
+				array[6] = m21;
+				array[7] = m31;
+
+				array[8] = m02;
+				array[9] = m12;
+				array[10] = m22;
+				array[11] = m32;
+
+				array[12] = m03;
+				array[13] = m13;
+				array[14] = m23;
+				array[15] = m33;
+				break;
+			default:
+				throw new IllegalArgumentException("Unsupported matrix ordering: " + order);
+		}
+	}
+
+	public static Matrix4 load(float[] array, Order order)
+	{
+		checkArgument(array.length >= 16, "Array has fewer than 16 elements (%d)", array.length);
+		switch(order)
+		{
+		   case ROW_MAJOR:
+			   return new Matrix4(
+				   array[ 0], array[ 1], array[ 2], array[3],
+			       array[ 4], array[ 5], array[ 6], array[ 7],
+			       array[ 8], array[ 9], array[10], array[11],
+			       array[12], array[13], array[14], array[15]
+			   );
+		   case COLUMN_MAJOR:
+			   return new Matrix4(
+				    array[0], array[4], array[ 8], array[12],
+			        array[1], array[5], array[ 9], array[13],
+			        array[2], array[6], array[10], array[14],
+			        array[3], array[7], array[11], array[15]
+			   );
+			default:
+				throw new IllegalArgumentException("Unsupported matrix ordering: " + order);
+		}
+	}
+
+	public static Matrix4 load(FloatBuffer buf, Order order)
+	{
+		checkArgument(buf.remaining() >= 16, "Buffer has fewer than 16 elements remaining (%s)", buf);
+		switch(order)
+		{
+			case ROW_MAJOR:
+				return new Matrix4(
+				    buf.get(), buf.get(), buf.get(), buf.get(),
+				    buf.get(), buf.get(), buf.get(), buf.get(),
+				    buf.get(), buf.get(), buf.get(), buf.get(),
+				    buf.get(), buf.get(), buf.get(), buf.get()
+				);
+			case COLUMN_MAJOR:
+				float m00 = buf.get();
+				float m10 = buf.get();
+				float m20 = buf.get();
+				float m30 = buf.get();
+
+				float m01 = buf.get();
+				float m11 = buf.get();
+				float m21 = buf.get();
+				float m31 = buf.get();
+
+				float m02 = buf.get();
+				float m12 = buf.get();
+				float m22 = buf.get();
+				float m32 = buf.get();
+
+				float m03 = buf.get();
+				float m13 = buf.get();
+				float m23 = buf.get();
+				float m33 = buf.get();
+
+				return new Matrix4(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33);
+			default:
+				throw new IllegalArgumentException("Unsupported matrix ordering: " + order);
+		}
 	}
 }
---
+++ src/test/groovy/galu/matrix/Matrix4Spec.groovy
@@ -0,0 +1,275 @@
+package galu.matrix
+
+import galu.vector.Vector4
+import spock.lang.Specification
+
+import java.nio.FloatBuffer
+
+import static spock.util.matcher.HamcrestSupport.that
+import static spock.util.matcher.HamcrestMatchers.*
+
+class Matrix4Spec extends Specification
+{
+	def "row count is 4"()
+	{
+		expect:
+		new Matrix4(*(1..16)).rowCount() == 4
+	}
+
+	def "column count is 4"()
+	{
+		expect:
+		new Matrix4(*(1..16)).columnCount() == 4
+	}
+	def "4 x 4 matrix determinant"()
+	{
+		given:
+			def matrix = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+		expect:
+			that matrix.determinant(), closeTo(0.0, 0.01)
+	}
+
+	def "4 x 4 matrix inverse"()
+	{
+		given:
+			def matrix = new Matrix4(1, 6, 2, 7, 5, 9, 4, 3, 2, 8, 1, 6, 0, 5, 3, 1)
+			def inverse = new Matrix4(0.025, 0.234, -0.097, -0.296, -0.234, -0.065, 0.280, 0.150, 0.305, 0.112, -0.433, 0.125, 0.252, -0.009, -0.103, -0.121)
+		expect:
+			assertClose(matrix.inverse(), inverse)
+	}
+
+	def "4 x 4 matrix not invertible"()
+	{
+		given:
+			def matrix = new Matrix4(*(1..16))
+		when:
+			matrix.inverse()
+		then:
+			thrown(IllegalStateException)
+	}
+
+	def "4 x 4 matrix negation"()
+	{
+		given:
+			def matrix = new Matrix4(*(1..16))
+			def expected = new Matrix4(*(-1..-16))
+		expect:
+			assertClose(matrix.negate(), expected)
+	}
+
+	def "4 x 4 matrix transpose"()
+	{
+		given:
+			def matrix = new Matrix4(
+				 1,  2,  3,  4,
+				 5,  6,  7,  8,
+				 9, 10, 11, 12,
+				13, 14, 15, 16
+			)
+			def expected = new Matrix4(
+				1, 5,  9, 13,
+				2, 6, 10, 14,
+				3, 7, 11, 15,
+				4, 8, 12, 16
+			)
+		expect:
+			assertClose(matrix.transpose(), expected)
+	}
+
+	def "4 x 4 matrix addition"()
+	{
+		given:
+			def a = new Matrix4(*(1..16))
+			def b = new Matrix4(*(1..16))
+			def sum = new Matrix4(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32)
+		expect:
+			assertClose(a.add(b), sum)
+	}
+
+	def "4 x 4 matrix subtraction"()
+	{
+		given:
+			def a = new Matrix4(*(1..16))
+			def b = new Matrix4(*(1..16))
+			def difference = new Matrix4(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+		expect:
+			assertClose(a.subtract(b), difference)
+	}
+
+	def "4 x 4 matrix multiplication"()
+	{
+		given:
+			def a = new Matrix4(*(1..16))
+			def b = new Matrix4(*(2..17))
+			def product = new Matrix4(100, 110, 120, 130, 228, 254, 280, 306, 356, 398, 440, 482, 484, 542, 600, 658)
+		expect:
+			assertClose(a.multiply(b), product)
+	}
+
+	def "4 x 4 matrix division"()
+	{
+		given:
+			def a = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+//			def b = new Matrix4(0.175, -0.179, 0.383, -0.209, -0.407, 0.367, -0.455, 0.368, -0.051, 0.072, -0.297, 0.254, 0.329, -0.184, 0.353, -0.309)
+			def b = new Matrix4(1, 3, 5, 7, 2, 8, 1, 9, 8, 4, 2, 1, 9, 3, 7, 0).inverse()
+			def quotient = new Matrix4(65, 43, 41, 28, 145, 115, 101, 96, 225, 187, 161, 164, 305, 259, 221, 232)
+		expect:
+			assertClose(a.divide(b), quotient)
+	}
+
+	def "4 x 4 matrix-scalar multiplication"()
+	{
+		given:
+			def matrix = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+			def scalar = 0.5
+			def product = new Matrix4(0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8)
+		expect:
+			assertClose(matrix.multiply(scalar), product)
+	}
+
+	def "4 x 4 element-wise multiplication"()
+	{
+		given:
+			def a = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+			def b = new Matrix4(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+			def product = new Matrix4(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32)
+		expect:
+			assertClose(a.elementMultiply(b), product)
+	}
+
+	def "4 x 4 element-wise division"()
+	{
+		given:
+			def a = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+			def b = new Matrix4(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+			def quotient = new Matrix4(0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8)
+		expect:
+			assertClose(a.elementDivide(b), quotient)
+	}
+
+	def "transform vector4"()
+	{
+		given:
+			def matrix = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+			def vector = new Vector4(1, 2, 3, 4)
+		    def transformed = new Vector4(30, 70, 110, 150)
+		expect:
+			assertClose(matrix.transform(vector), transformed)
+	}
+
+	def "row-major storage"()
+	{
+		given:
+			def matrix = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+		    def array = new float[16]
+			def buffer = FloatBuffer.allocate(16)
+		when:
+			matrix.store(array, Matrix.Order.ROW_MAJOR)
+			matrix.store(buffer, Matrix.Order.ROW_MAJOR)
+			buffer.flip()
+		then:
+		    16.times {
+			    array[it] == it + 1
+			    buffer.get() == it + 1
+		    }
+	}
+
+	def "column-major storage"()
+	{
+		given:
+			def matrix = new Matrix4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+			def array = new float[16]
+			def buffer = FloatBuffer.allocate(16)
+		when:
+			matrix.store(array, Matrix.Order.COLUMN_MAJOR)
+			matrix.store(buffer, Matrix.Order.COLUMN_MAJOR)
+			buffer.flip()
+		then:
+		    Arrays.equals(array, [1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16] as float[])
+			buffer.equals(FloatBuffer.wrap([1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16] as float[]))
+	}
+
+	def "store into too-small array"()
+	{
+		when:
+			new Matrix4(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).store(new float[10], Matrix.Order.COLUMN_MAJOR)
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	def "store into too-small buffer"()
+	{
+		when:
+			new Matrix4(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).store(FloatBuffer.allocate(10), Matrix.Order.COLUMN_MAJOR)
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	def "load row-major"()
+	{
+		when:
+			def array = 1..16 as float[]
+		    def buffer = FloatBuffer.wrap(array)
+			def expected = new Matrix4(*(1..16))
+		then:
+		    assertClose(Matrix4.load(array, Matrix.Order.ROW_MAJOR), expected)
+			assertClose(Matrix4.load(buffer, Matrix.Order.ROW_MAJOR), expected)
+	}
+
+	def "load column-major"()
+	{
+		when:
+			def array = [1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16] as float[]
+			def buffer = FloatBuffer.wrap(array)
+			def expected = new Matrix4(*(1..16))
+		then:
+		    assertClose(Matrix4.load(array, Matrix.Order.COLUMN_MAJOR), expected)
+			assertClose(Matrix4.load(buffer, Matrix.Order.COLUMN_MAJOR), expected)
+	}
+
+	def "load from too-small array"()
+	{
+		when:
+			Matrix4.load(new float[10], Matrix.Order.COLUMN_MAJOR)
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	def "load from too-small buffer"()
+	{
+		when:
+			Matrix4.load(FloatBuffer.allocate(10), Matrix.Order.COLUMN_MAJOR)
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	//TODO: move all the assertClose methods in test classes into one helper
+
+	void assertClose(Matrix4 a, Matrix4 b, float delta = 0.01)
+	{
+		assert that(a.m00, closeTo(b.m00, delta))
+		assert that(a.m01, closeTo(b.m01, delta))
+		assert that(a.m02, closeTo(b.m02, delta))
+		assert that(a.m03, closeTo(b.m03, delta))
+		assert that(a.m10, closeTo(b.m10, delta))
+		assert that(a.m11, closeTo(b.m11, delta))
+		assert that(a.m12, closeTo(b.m12, delta))
+		assert that(a.m13, closeTo(b.m13, delta))
+		assert that(a.m20, closeTo(b.m20, delta))
+		assert that(a.m21, closeTo(b.m21, delta))
+		assert that(a.m22, closeTo(b.m22, delta))
+		assert that(a.m23, closeTo(b.m23, delta))
+		assert that(a.m30, closeTo(b.m30, delta))
+		assert that(a.m31, closeTo(b.m31, delta))
+		assert that(a.m32, closeTo(b.m32, delta))
+		assert that(a.m33, closeTo(b.m33, delta))
+	}
+
+	void assertClose(Vector4 a, Vector4 b, float delta = 0.01)
+	{
+		assert that(a.x, closeTo(b.x, delta))
+		assert that(a.y, closeTo(b.y, delta))
+		assert that(a.z, closeTo(b.z, delta))
+		assert that(a.w, closeTo(b.w, delta))
+	}
+}
